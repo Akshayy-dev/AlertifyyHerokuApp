f63b7456855d73605a6f4659092868f8a557ca94
indcoders <72201055+indcoders@users.noreply.github.com>
2022-03-28 15:12:06 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -4,43 +4,81 @@ import os
 
 TOKEN = os.environ.get("TELEGRAM_ID")
 
+# def start(update, context):
+#     username = update.message.chat.first_name
+#     msg = "Hi " + username + "! Please follow the instructions on the Alertifyy app to add Alertifyy Bot in your group or channel.\n\nInstall the from: http://google.com/"
+#     # if Filters.chat_type == Filters.private:
+#     context.bot.sendMessage(update.message.chat_id, msg)
+#
+#
+# def getchatid(update, context):
+#     print(update.message.chat_id)
+#     # if update.message.cha != "private":
+#
+#     context.bot.sendMessage(update.effective_message.chat_id, update.effective_message.chat_id)
+#
+#     if "/getchatid" in str(update.effective_message.text):
+#         context.bot.sendMessage(update.effective_chat, update.effective_chat)
+#
+#
+# def error(update, context):
+#     msg = "Oops! Error encountered."
+#     context.bot.sendMessage(update.message.chat_id, msg)
+#
+#
+# def main():
+#     updater = Updater(token=TOKEN)
+#     dp = updater.dispatcher
+#
+#     dp.add_handler(CommandHandler("start", start, filters=Filters.private))
+#     dp.add_handler(CommandHandler("getchatid", getchatid))  # ,filters=Filters.group))
+#     dp.add_handler(MessageHandler(Filters.text, getchatid))
+#     dp.add_error_handler(error)
+#
+#     updater.start_webhook(listen="0.0.0.0",
+#                           port=os.environ.get("PORT", 443),
+#                           url_path=TOKEN, webhook_url="https://alertifyy-telegram-bot.herokuapp.com/" + TOKEN)
+#     updater.idle()
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-def start(update, context):
-    username = update.message.chat.first_name
-    msg = "Hi " + username + "! Please follow the instructions on the Alertifyy app to add Alertifyy Bot in your group or channel.\n\nInstall the from: http://google.com/"
-    # if Filters.chat_type == Filters.private:
-    context.bot.sendMessage(update.message.chat_id, msg)
+import os
+import telebot
+
+# print(my_secret)
+bot = telebot.TeleBot(TOKEN)
 
 
-def getchatid(update, context):
-    print(update.message.chat_id)
-    # if update.message.cha != "private":
-
-    context.bot.sendMessage(update.effective_message.chat_id, update.effective_message.chat_id)
-
-    if "/getchatid" in str(update.effective_message.text):
-        context.bot.sendMessage(update.effective_chat, update.effective_chat)
+# telebot.TeleBot
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message, "Howdy!")
 
 
-def error(update, context):
-    msg = "Oops! Error encountered."
-    context.bot.sendMessage(update.message.chat_id, msg)
+@bot.channel_post_handler(commands=['getchatid'])
+def getchatid(message):
+    print(message.chat.id)
+    if message.chat.type != "private":
+        bot.send_message(message.chat.id, message.chat.id)
 
 
-def main():
-    updater = Updater(token=TOKEN)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start, filters=Filters.private))
-    dp.add_handler(CommandHandler("getchatid", getchatid))  # ,filters=Filters.group))
-    dp.add_handler(MessageHandler(Filters.text, getchatid))
-    dp.add_error_handler(error)
-
-    updater.start_webhook(listen="0.0.0.0",
-                          port=os.environ.get("PORT", 443),
-                          url_path=TOKEN, webhook_url="https://alertifyy-telegram-bot.herokuapp.com/" + TOKEN)
-    updater.idle()
+@bot.message_handler(commands=['getchatid'])
+def getchatid(message):
+    print(message.chat.id)
+    if message.chat.type != "private":
+        # print(message.chat.id)
+        bot.reply_to(message, message.chat.id)
 
 
-if __name__ == '__main__':
-    main()
+@bot.message_handler(commands=['start'])
+def start(message):
+    print(message.chat.id)
+    if message.chat.type == "private":
+        # print(message.chat.id)
+        bot.reply_to(message,
+                     "Please follow the instructions on the Alertifyy app to add Alertifyy Bot in your group or channel.\n\nInstall the from: http://google.com/")
+
+
+bot.polling()
