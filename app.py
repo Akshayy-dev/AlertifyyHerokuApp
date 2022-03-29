7d8032561728e8b36db23b7c3a8e1e7ff18dfabb
indcoders <72201055+indcoders@users.noreply.github.com>
2022-03-29 13:07:42 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
     dp.add_handler(CommandHandler("getchatid", getchatid))  # ,filters=Filters.group))
     #dp.add_handler(MessageHandler(Filters.text & (~Filters.command), getchatid))
 
-    dp.add_handler(MessageHandler(Filters.regex('^getchatid$'), getchatid))
+    dp.add_handler(MessageHandler(Filters.regex('^/getchatid$'), getchatid))
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0",
