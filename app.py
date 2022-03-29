deb80814ed20885427ccedfc1387d78dfc310506
indcoders <72201055+indcoders@users.noreply.github.com>
2022-03-29 12:56:24 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def getchatid(update, context):
 
 def error(update, context):
     msg = "Oops! Error encountered."
-    context.bot.sendMessage(update.effective_chat.id, msg)
+    context.bot.sendMessage(update.effective_chat.id, context.error)
 
 
 def main():
